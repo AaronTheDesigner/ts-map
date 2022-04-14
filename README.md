909168e82454b8061d6bf4d4063063dc3ddff8b9
Aaron Toliver <flux.charlotte@gmail.com>
2022-04-14 14:10:25 -0400

    company class added

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# FILE STRUCTURE NOTES
+# TS Map
+small map for typescript practice. uses faker and random generation. 
+
+### extra notes
+ * use Definitely Typed to find type declaration files or...
+ * look up on npm.org
+
+### FILE STRUCTURE NOTES
  * every class should have it's own file
  * class for user, company, map (Map.ts, User.ts, Company.ts)
  * a file whose primary purpose is to create an export (class) is capitalized.
--- src/User.ts
+++ src/User.ts
@@ -1,13 +1,21 @@
 import faker from 'faker'
 
-class User {
+export class User {
     name: string;
+    // object within class needs to be initialized
     location: {
         lat: number;
         lng: number;
     };
 
     constructor() {
-        
+        // generate a first name
+        this.name = faker.name.firstName();
+        //mirror location declaration above
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude()),
+        }
+
     }
 }
